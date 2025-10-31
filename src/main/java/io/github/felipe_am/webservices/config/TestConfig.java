b6ba18714214c52112b06cb8be9e3f89daa32919
Felipe Alencar <felipealencar465@gmail.com>
2025-10-30 20:36:22 -0400

    Payment, one-to-one association

--- src/main/java/io/github/felipe_am/webservices/config/TestConfig.java
+++ src/main/java/io/github/felipe_am/webservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import io.github.felipe_am.webservices.entities.Category;
 import io.github.felipe_am.webservices.entities.Order;
 import io.github.felipe_am.webservices.entities.OrderItem;
+import io.github.felipe_am.webservices.entities.Payment;
 import io.github.felipe_am.webservices.entities.Product;
 import io.github.felipe_am.webservices.entities.User;
 import io.github.felipe_am.webservices.entities.enums.OrderStatus;
@@ -89,6 +90,15 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		/*
+		 * para salvar objetos de classe dependentes em associações 1 p/ 1, utiliza-se
+		 * o repositório da classe dominante, conforme feito abaixo
+		 */
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T20:53:07Z"), o1);		
+		o1.setPayment(pay1);		
+		orderRepository.save(o1);
 		
 	}
 	
